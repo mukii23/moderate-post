3cba0476b435ce8e4dc554a141661849aa4b1d1e
Mukii <mukii2307@gmail.com>
2019-07-01 16:42:00 +0530

    Create post_data.php

---
+++ widget/post_data.php
@@ -0,0 +1,11 @@
+<?php
+
+/*******
+ *** Modules file for widget.
+ *******/
+
+function get_image_id($image_url) {
+    global $wpdb;
+    $attachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid='%s';", $image_url )); 
+    return $attachment[0]; 
+}
